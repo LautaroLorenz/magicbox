3b5b335afa15322b3daa7ad5d266f4cbd4ca25ae
Maximiliano <maximilianomercado.n@gmail.com>
2019-06-01 00:44:17 -0300

    Nueva version conexion bt

--- Android/MagicBox/app/src/main/java/com/example/magicbox/magicbox/MainActivity.java
+++ Android/MagicBox/app/src/main/java/com/example/magicbox/magicbox/MainActivity.java
@@ -1,80 +1,109 @@
 package com.example.magicbox.magicbox;
 
-
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
 import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 
-public class MainActivity extends Activity  {
-    Button b1,b2,b3,b4;
-    private BluetoothAdapter BA;
-    private Set<BluetoothDevice>pairedDevices;
-    ListView lv;
+public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
+    private ListView listView;
+    private ArrayAdapter arrayAdapter;
+    private BluetoothAdapter bt;
+    private BluetoothDevice device;
+    private BluetoothSocket tmp = null;
+    private BluetoothSocket mmSocket = null;
+    UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        b1 = (Button) findViewById(R.id.button);
-        b2=(Button)findViewById(R.id.button2);
-        b3=(Button)findViewById(R.id.button3);
-        b4=(Button)findViewById(R.id.button4);
 
-        BA = BluetoothAdapter.getDefaultAdapter();
-        lv = (ListView)findViewById(R.id.listView);
-    }
+        bt = BluetoothAdapter.getDefaultAdapter();
 
-    public void on(View v){
-        if (!BA.isEnabled()) {
-            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnOn, 0);
-            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+        Button btnOn = findViewById(R.id.btnOn);
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!bt.isEnabled()) {
+                    Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(turnOn, 0);
+                    Toast.makeText(getApplicationContext(), "Bluetooth encendido",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "El bluetooth ya se encuentra encendido", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+
+        Button btnGetDevices = findViewById(R.id.btnGet);
+        btnGetDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Set<BluetoothDevice> pairedDevices = bt.getBondedDevices();
+                ArrayList listDevice = new ArrayList();
+
+                if (pairedDevices.size() > 0) {
+
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceMAC = device.getAddress();
+                        listDevice.add(deviceName + "\n" + deviceMAC);
+                    }
+                }
+                listView = findViewById(R.id.deviceList);
+                arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, listDevice);
+                listView.setAdapter(arrayAdapter);
+
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                        String data[] = ((String) listView.getItemAtPosition(position)).split("\n");
+
+                        BluetoothConnect conectar = new BluetoothConnect(data[1], device, bt);
+
+                        conectar.run();
+                        mmSocket = conectar.getSocket();
+
+                        if (mmSocket.isConnected()) {
+                            Toast.makeText(MainActivity.this, "Conectado", Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "Bluetooth conectado");
+                            Intent i = new Intent(MainActivity.this, ProductoActivity.class);
+                            startActivity(i);
+
+                        } else {
+
+                            Toast.makeText(MainActivity.this, "No Conectado", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, "Bluetooth no conectado");
+                        }
+
+                    }
+                });
+            }
+        });
+
         }
-    }
 
-    public void off(View v){
-        BA.disable();
-        Toast.makeText(getApplicationContext(), "Turned off" ,Toast.LENGTH_LONG).show();
-    }
-
-
-    public  void visible(View v){
-        Intent getVisible = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(getVisible, 0);
-    }
-
-    public void ok(View v){
-
-        Intent i = new Intent(this, ProductoActivity.class);
-        startActivity(i);
 
     }
-    public void list(View v){
-        pairedDevices = BA.getBondedDevices();
-
-        ArrayList list = new ArrayList();
-
-        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
-        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
-
-        final ArrayAdapter adapter = new  ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-
-        lv.setAdapter(adapter);
-    }
-}
